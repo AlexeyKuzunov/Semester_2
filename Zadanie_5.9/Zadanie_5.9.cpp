136495f10723e64f29d740cdb607b222e29b5268
AlexeyKuzunov <a.kuzunov@gmail.com>
2022-04-25 21:12:50 +0300

    Оси+

--- Zadanie_5.9/Zadanie_5.9.cpp
+++ Zadanie_5.9/Zadanie_5.9.cpp
@@ -18,10 +18,14 @@ using namespace std;
 int i = 0;
 Points ap_f, ap_p; //массивы в физической и пиксельной системах
 void calculatepoints(Points& ap_tmp, int x, int y, int V, int deg);
+int dx = 0, dy = 0;
+My_point mp = { 0.0, 0.0 };
 
 int WINAPI DlgProc(HWND hDlg, WORD wMsg, WORD wParam, DWORD)
 {
-	PAINTSTRUCT ps;	
+	PAINTSTRUCT ps;		
+	My_point tmpp;
+
 	if (wMsg == WM_CLOSE || wMsg == WM_COMMAND && wParam == IDOK) {
 		EndDialog(hDlg, 0);
 	}
@@ -30,18 +34,27 @@ int WINAPI DlgProc(HWND hDlg, WORD wMsg, WORD wParam, DWORD)
 			/* Узнаем размер окна (ось OY направлена вниз): */
 			RECT rc;
 			GetClientRect(hDlg, &rc);
-			int dx = rc.right - rc.left;
-			int dy = rc.bottom - rc.top;
-			My_point mp = { 0.0, (double)dy }, tmpp;
+			dx = rc.right - rc.left;
+			dy = rc.bottom - rc.top;
+
+			//Для построения осей уменьшаем значения x и y массива на 60 единиц			
+			mp = { -60.0, (double)dy - 60 };
+
 			/* Преобразование массива из физической в пиксельную системы*/
 
 			for (int i = 0; i < ap_f.size(); i++) {
-				tmpp = mp - ap_f[i];
+				//Т.к. начало осей координат находится в верхнем левом углу формы
+				//отнимаем значение оси y в массиве от высоты формы 
+				tmpp = mp - ap_f[i];				
 				double ty = tmpp.gety();
+				// Пока  график не достиг оси х либо границы ээкрана
 				if ((tmpp.getx() < dx)&&(ty > 0))
+					//заносим значения в пиксельный массив
 					ap_p.add(tmpp.getx(), tmpp.gety());
 				else {
+					//иначе заносим еще одно значение в массив пикселей
 					ap_p.add(tmpp.getx(), tmpp.gety());
+					//и выходим из цикла
 					break;
 				}
 					
@@ -55,6 +68,49 @@ int WINAPI DlgProc(HWND hDlg, WORD wMsg, WORD wParam, DWORD)
 				HPEN hPen = (HPEN)CreatePen(PS_SOLID, 1, RGB(0, 0, 255));
 				HPEN hOldPen = (HPEN)SelectObject(ps.hdc, hPen);
 				POINT ptOld;
+
+				/* Рисуем ось абсцисс (х) */
+				MoveToEx(ps.hdc, 60, dy-60, NULL);
+				LineTo(ps.hdc, dx, mp.gety());
+
+				/* Рисуем ось ординат (y) */
+				MoveToEx(ps.hdc, 60, 0, NULL);
+				LineTo(ps.hdc, 60, dy - 60);
+
+				TCHAR a[8];
+
+				// ось х - время
+				for (int timer = 0; timer <= 7; timer++) 
+				{
+					Rectangle(ps.hdc, 60 + timer * 80, mp.gety(), 60 + timer * 80 + 2, mp.gety() + 10); //риски на оси
+					SetBkMode(ps.hdc, TRANSPARENT); //за надписью нет цвета
+					SetTextColor(ps.hdc, RGB(0, 0, 0)); //цвет текста
+					swprintf_s(a, 2, L"%d", timer); // формируем текст в массиве а
+					TextOut(ps.hdc, 60 + timer * 80, mp.gety() + 10, a, 1); //выводим текст по оси х
+
+				}
+
+				// ось y 
+				for (int timer = 0; timer >= -6; timer--)
+				{
+					Rectangle(ps.hdc, 50, (dy - 58) + timer*80, 60, (dy - 60) + timer * 80); //риски на оси
+					SetBkMode(ps.hdc, TRANSPARENT); //за надписью нет цвета
+					SetTextColor(ps.hdc, RGB(0, 0, 0)); //цвет текста
+					swprintf_s(a, 2, L"%d", -1 * timer); // формируем текст в массиве а
+					TextOut(ps.hdc, 40, (dy - 60) + timer * 80, a, 1); //выводим текст по оси х
+
+				}
+
+				//подписи к оси х и у
+				SetBkMode(ps.hdc, TRANSPARENT);
+				SetTextColor(ps.hdc, RGB(0, 0, 0));
+
+				swprintf_s(a, 8, L"time, t");
+				TextOut(ps.hdc, dx - 150, dy - 55, a, 8);
+
+				swprintf_s(a, 5, L"S, m");
+				TextOut(ps.hdc, 5, 5, a, 5);
+
 				MoveToEx(ps.hdc, (int)ap_p[0].getx(), (int)ap_p[0].gety(), &ptOld);
 				for (int i = 0; i < ap_p.size(); i++) {
 					LineTo(ps.hdc, (int)ap_p[i].getx(), (int)ap_p[i].gety());
@@ -103,7 +159,6 @@ int main()
 	int	V = 1,
 		deg = 0;	
 	int x, y = 0;	
-
 	/* Ввод параметров задачи: */
 
 	cout << "Введите через Enter начальные координаты тела х0 и y0:\n" << flush;
@@ -115,6 +170,8 @@ int main()
 
 
 	calculatepoints(ap_f, x, y, V, deg);
+
+
 
 	/* Вывод на экран массива */
 	cout << ap_f << endl;
--- Zadanie_6.9/mydeque.h
+++ Zadanie_6.9/mydeque.h
@@ -4,9 +4,28 @@
 
 using namespace std;
 
-template <class T> class Mydeque { 
+template <class T> class Mydeque
+{
 private:
-
-	size_t index; //индекс текущего элемента
+    vector<T> begindq;
+    vector<T> enddq;
+    size_t index; //
 public:
+    Mydeque(int value) {
+        begindq.reserve(value);
+        enddq.reserve(value);
+    };
+    ~Mydeque() {};
+    void push_back(const T element) { enddq.push_back(element); }
+    void push_front(const T element) { begindq.push_back(element); }
+    void push_front(const T* element, int length) {
+        for (int i = 0; i < length; i++) {
+            begindq.push_back(element[i]);
+        }
+    }
+
+
+
+    int size() { return (begindq.size() + enddq.size()); }
+
 };
