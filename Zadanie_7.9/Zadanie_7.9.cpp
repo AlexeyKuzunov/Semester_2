a571bcaab5dd2df165cbffd76ff4c39c0f5de65a
AlexeyKuzunov <a.kuzunov@gmail.com>
2022-04-26 23:17:57 +0300

    7.9 final

---
+++ Zadanie_7.9/Zadanie_7.9.cpp
@@ -0,0 +1,50 @@
+﻿/*
+Создать функцию, убирающую из текстового файла все цифры. Функция принимает
+имена входного и выходного файлов.
+Файл должен считываться по строкам с использованием функций библиотеки <fstream>, строки должны
+записываться в контейнер string библиотеки STL.
+*/ 
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib> // для использования функции exit()
+#include <string>
+#include <Windows.h>
+
+using namespace std;
+
+int NoNumber(char *infile, char *outfile) {
+    wstring line; // - хранилище временных строк 
+    wifstream in("in.txt"); // - входной файл (нужно создать в папке с програмой in.txt)
+    wofstream out("out.txt"); // - выходной файл (будет создан сам)
+
+    while (getline(in, line)) {
+        for (unsigned int c = 0; c < line.length(); c++){
+            if (c >= line.length()) // - из-за изменений размера строки
+                break;
+            if (isdigit(line[c])) {// - если найдена цифра            
+                line.erase(line.begin() + c); // - удаление символа
+                c--;
+            }
+        }
+        out << line << '\n';
+    }
+    cout << "Текст обработан и записан в out.txt.\n";
+    in.close();
+    out.close();
+    return 0;
+}
+
+int main(int argc, char* argv[])
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	if (argc == 1) { // если в аргументах только имя программы
+		cout << "no arguments!" << endl; // выводим, что нет аргументов
+	}
+	else 
+        return NoNumber(argv[1], argv[2]);
+
+}
+
