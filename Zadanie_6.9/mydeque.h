e81a5cc1d1f62979f9892bd5787866ff53c56ad4
AlexeyKuzunov <a.kuzunov@gmail.com>
2022-04-26 20:31:19 +0300

    6.9 end

--- Zadanie_6.9/mydeque.h
+++ Zadanie_6.9/mydeque.h
@@ -16,27 +16,47 @@ public:
         enddq.reserve(value);
     };
     ~Mydeque() {};
-    void push_back(const T element) { enddq.push_back(element); }
-    void push_front(const T element) { begindq.push_back(element); }
-    void push_front(const T* element, int length) {
-        for (int i = 0; i < length; i++) {
-            begindq.push_back(element[i]);
-        }
+    //Вставка одного элемента в конец очереди
+    void push_back(const T element) {
+        if (enddq.capacity() == enddq.size())
+            enddq.reserve(enddq.size() * 2);
+        enddq.push_back(element);
     }
+    //Вставка массива длинной length в конец очереди
     void push_back(const T* element, int length) {
+        if (enddq.capacity() == enddq.size())
+            enddq.reserve(enddq.size() * 2);
         for (int i = 0; i < length; i++) {
             enddq.push_back(element[i]);
         }
     }
+
+    //Вставка одного элеиента в начало очереди
+    void push_front(const T element) {
+        if (begindq.capacity() == begindq.size())
+            begindq.reserve(begindq.size() * 2);
+        begindq.push_back(element);
+    }
+
+    //Вставка массива в начало очереди
+    void push_front(const T* element, int length) {
+        if (begindq.capacity() == begindq.size())
+            begindq.reserve(begindq.size() * 2);
+        for (int i = 0; i < length; i++) {
+            begindq.push_back(element[i]);
+        }
+
+    }
+    //Длинна очереди
     int size() { return (begindq.size() + enddq.size()); }
 
     friend ostream& operator<< (std::ostream& out, const Mydeque& r) {
-       
-        r.begindq<T> ::const_iterator it;
-        it = r.begindq.begin();
-        while (it != begindq.end()) {   // пока итератор не достигнет последнего элемента            
-            out << *it << " "; // выводим значение элемента, на который указывает итератор
-            ++it; // и переходим к следующему элементу
-        }
+        for (int i = 0; i < r.begindq.size(); i++)
+            out << r.begindq[i];
+
+        for (int i = 0; i < r.enddq.size(); i++)
+            out << r.enddq[i];
+
+        return out;
     }
 };
